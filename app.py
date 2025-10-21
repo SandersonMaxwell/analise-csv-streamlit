20390e9b183ad66fa2ffbd10d079bba174b5a8b8
Sanderson Maxwell <80972527+SandersonMaxwell@users.noreply.github.com>
2025-10-21 11:57:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -251,25 +251,9 @@ with abas[1]:
                 st.markdown(f"**📈 Lucro total (reais + gratuitas):** {lucro_colorido(lucro_jogo)}")
                 st.markdown("---")
 
-                linhas_relatorio.append({
-                    "Jogo": jogo,
-                    "Total Apostado": total_jogo_apostado,
-                    "Total Pago": total_jogo_payout,
-                    "Lucro do Jogador": lucro_jogo
                 })
 
-            # -----------------------------
-            # DataFrame final para download
-            # -----------------------------
-            df_relatorio = pd.DataFrame(linhas_relatorio)
-            relatorio_csv = gerar_relatorio_csv(df_relatorio)
-
-            st.download_button(
-                label="📥 Baixar Relatório Completo",
-                data=relatorio_csv,
-                file_name="relatorio_jogos.csv",
-                mime="text/csv"
-            )
+           
 
             # -----------------------------
             # Análise com Net Deposit
@@ -289,3 +273,4 @@ with abas[1]:
 
         except Exception as e:
             st.error(f"Ocorreu um erro ao processar o arquivo: {e}")
+
